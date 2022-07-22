2226c415a632b3b6180a2ba6bf43dc8d60289d08
Stefano Longhi <34969+abaddon@users.noreply.github.com>
2022-07-23 00:43:33 +0100

    update versions
    * update jacksonModuleKotlinVersion version
    * update eventStoreDB Version

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,11 +15,11 @@ object Meta {
 object Versions {
     const val kcqrsCoreVersion = "0.0.7"
     const val kcqrsTestVersion = "0.0.3"
-    const val eventStoreDBVersion = "1.0.0"
+    const val eventStoreDBVersion = "3.0.1"
     const val slf4jVersion = "1.7.25"
     const val kotlinVersion = "1.6.0"
     const val kotlinCoroutineVersion = "1.6.0"
-    const val jacksonModuleKotlinVersion = "2.13.0"
+    const val jacksonModuleKotlinVersion = "2.13.3"
     const val testContainerVersion = "1.16.3"
     const val junitJupiterVersion = "5.7.0"
     const val jacocoToolVersion = "0.8.7"
